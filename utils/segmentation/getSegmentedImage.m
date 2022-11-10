0087944b7ad772227970edcf9c17234d74cc058a
billyjuliux <billyjulius01@gmail.com>
2022-11-10 22:37:58 +0700

    improve segmentation

--- utils/segmentation/getSegmentedImage.m
+++ utils/segmentation/getSegmentedImage.m
@@ -11,8 +11,10 @@ function res = getSegmentedImage(img, edge)
 
     % Dilate the image
     mask = imdilate(edge, [ ...
-        strel('line', 2, 0), ...
-        strel('line', 2, 90), ...
+        strel('line', 4, 0), ....
+        strel('line', 4, 90), ...
+        strel('line', 4, 135), ...
+        strel('disk', 6), ...
     ]);
 
     % Fill the area closed by edges
@@ -21,9 +23,8 @@ function res = getSegmentedImage(img, edge)
     % Remove border objects
     mask = imclearborder(mask, 4);
 
-    % Smoothen the objects, twice
-    mask = imerode(mask, strel('diamond', 1));
-    mask = imerode(mask, strel('diamond', 1));
+    % Smoothen the objects
+    mask = imerode(mask, strel('diamond', 15));
 
     % Removes all connected objects with less than 1000 pixels
     mask = bwareaopen(mask, 1000);
